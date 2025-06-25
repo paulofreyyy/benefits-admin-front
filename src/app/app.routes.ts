2433d034ba74e1a669eb44156d24ffa4f5076591
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-06-25 17:08:48 -0300

    feat: creating benefits page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { VouchersComponent } from './pages/vouchers/vouchers.component';
 import { CreateVoucherComponent } from './pages/vouchers/create/create-voucher.component';
 import { VouchersHistoryComponent } from './pages/vouchers/history/history-vouchers.component';
+import { BenefitsComponent } from './pages/benefits/benefits.component';
 
 export const routes: Routes = [
     // { path: '', component: HomeComponent },
     { path: 'usuarios', component: UsersComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'perfil', component: ProfileComponent,canActivate: [AuthGuard] },
+    { path: 'beneficios', component: BenefitsComponent,canActivate: [AuthGuard] },
     { path: 'vouchers', component: VouchersComponent,canActivate: [AuthGuard] },
     { path: 'vouchers-history', component: VouchersHistoryComponent,canActivate: [AuthGuard] },
     { path: 'create-voucher', component: CreateVoucherComponent,canActivate: [AuthGuard] },
---
+++ src/app/pages/benefits/benefits.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-benefits',
+    templateUrl: './benefits.component.html',
+    styleUrls: ['./benefits.component.css'],
+    imports: [
+        CommonModule,
+    ],
+})
+export class BenefitsComponent { }
